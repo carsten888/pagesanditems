8dc75fcebc7683992956e1b8f6f98c1bb2c3b4ea
Carsten Engel <info@pagesanditems.com>
2012-05-04 14:59:43 +0200

    3rd upload attempt

---
+++ site/controller.php
@@ -0,0 +1,34 @@
+<?php
+/**
+* @version		2.1.5
+* @package		PagesAndItems com_pagesanditems
+* @copyright	Copyright (C) 2006-2011 Carsten Engel. All rights reserved.
+* @license		http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @author		www.pages-and-items.com
+*/
+
+// no direct access
+defined('_JEXEC') or die();
+
+jimport('joomla.application.component.controller');
+
+class pagesanditemsController extends JController{
+
+	//var $helper;
+
+	function display(){		
+		
+		//require_once(JPATH_COMPONENT_ADMINISTRATOR.DS.'helpers'.DS.'pagesanditems.php');
+		//$this->helper = new PagesAndItemsHelper();
+		
+		parent::display();
+				
+	}
+	
+	function __construct(){			
+		
+		parent::__construct();	
+				
+	}
+}
+?>
---
+++ site/views/itemtypeselect/view.html.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* @version		2.1.5
+* @package		PagesAndItems com_pagesanditems
+* @copyright	Copyright (C) 2006-2011 Carsten Engel. All rights reserved.
+* @license		http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* @author		www.pages-and-items.com
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die();
+
+jimport( 'joomla.application.component.view');
+
+class PagesAndItemsViewItemtypeselect extends JView{
+
+	function display( $tpl = null ){
+	
+		//get backend language files
+		$lang = &JFactory::getLanguage();
+		$lang->load('com_pagesanditems', JPATH_ADMINISTRATOR.DS, null, false) || $lang->load('com_pagesanditems', JPATH_ADMINISTRATOR, $lang->getDefault(), false, false);
+		$path = PagesAndItemsHelper::getDirCSS();
+		JHtml::stylesheet($path.'/pagesanditems2.css');
+		require_once(JPATH_COMPONENT_ADMINISTRATOR.DS.'helpers'.DS.'pagesanditems.php');
+		//$helper = new PagesAndItemsHelper();		
+		//$this->assignRef('helper', $helper);
+		
+		parent::display($tpl);
+	}
+}
+
+?>
